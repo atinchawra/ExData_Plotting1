86f6a73db8c85a4cfa343a12a9fa24bf46e24e92
atinchawra <me@atinchawra.com>
2014-10-09 13:26:10 +0530

    init

---
+++ plot3.R
@@ -0,0 +1,14 @@
+library(lubridate)
+library(data.table)
+download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile="exdata_data_household_power_consumption.zip",method="internal")
+f<-unz("exdata_data_household_power_consumption.zip","household_power_consumption.txt")
+d<-read.csv(f,header=T,sep=";",na.strings="?",colClasses=c(rep("character",2),rep("numeric",7)))
+d<-data.table(d)
+d<-d[Date=='1/2/2007' | Date=='2/2/2007']
+d<-transform(d,DateTime=paste(Date,Time))
+d<-transform(d,DateTime=dmy_hms(DateTime))
+
+#3
+with(d,plot(DateTime,Sub_metering_1,col="black",type="l"))
+with(d,lines(DateTime,Sub_metering_2,col="red",type="l"))
+with(d,lines(DateTime,Sub_metering_3,col="blue",type="l"))
